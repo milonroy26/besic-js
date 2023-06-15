a59caae7a3e02be17eb0e2f6ab7982d98e32278e
milon roy <milonchandro35@gmail.com>
2023-06-15 17:37:25 +0600

    math method practice done

---
+++ module-2/math.js
@@ -0,0 +1,31 @@
+// Math.round()
+const num = 7.5;
+// console.log(Math.round(num)); //output: .4 number besi hle tkn round kre number ta ke 7 korbe.
+
+// Math.ceil()
+// console.log(Math.ceil(num)); //output: ceil .1 hle number ta ke ceil kre 1besi count korbe.
+
+//Math.floor()
+// console.log(Math.floor(num)); //output: .por jei number thak na keno ata kokno count korbe na.
+
+// Math.trunc()
+// console.log(Math.trunc(num)); //output: floor seme kaj ta kre.
+
+//Math.sign()
+// console.log(Math.sign(num)); //🫡 confeution ace?
+
+//Math.abs()
+// console.log(Math.abs(num)); //output `-1` number take positive value return kre.
+
+//Math.min() and Math.max()
+// console.log(Math.min(0, 150, 30, 20, -8, -200)); //sob thake cotton number take return korbe.
+// console.log(Math.max(0, 150, 30, 20, -8, -200)) //big number ta return korbe.
+
+// Math.random()
+// console.log(Math.random(num)); //math randorm method randrom number dei.
+
+//math.log()
+console.log(Math.log(1)); //🫡confeution?
+
+
+
